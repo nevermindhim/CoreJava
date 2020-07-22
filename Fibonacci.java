2c0ea9920d8483b003c70de96f0c71ecc3ac533a
verytinydever <tinydever@gmail.com>
2020-07-22 21:30:52 +0545

    for loop

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,9 +4,14 @@ public class Fibonacci {
 	public static void main(String [] args) {
 		System.out.println("Program for fibonacci series");
 		int n=10;
+		int i;
 		int firstFibNo =0;
 		int secondFibNo =1;
-		
+		int temp=firstFibNo;
+		for (i=0;i<=n;i++) {
+			System.out.println(temp);
+			
+		}
 	}
 
 }
