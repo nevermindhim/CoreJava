5a10b21ec3772e6c311beb9d53a0d32c751d1f31
verytinydever <tinydever@gmail.com>
2020-07-22 21:49:05 +0545

    user input

--- Palindrome.java
+++ Palindrome.java
@@ -4,7 +4,9 @@ import java.util.*;
 public class Palindrome {
 	public static void main (String[] args) {
 		System.out.println("Program for palindrome");
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number to check for palindrome");
+		sc.close();
 	}
 
 }
