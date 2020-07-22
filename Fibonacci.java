c943ff3ead567fec5dc85e0a5cf2d9abad64dc9a
verytinydever <tinydever@gmail.com>
2020-07-22 21:27:10 +0545

    declare variables

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,6 +3,10 @@ package com.test;
 public class Fibonacci {
 	public static void main(String [] args) {
 		System.out.println("Program for fibonacci series");
+		int n=10;
+		int firstFibNo =0;
+		int secondFibNo =1;
+		
 	}
 
 }
