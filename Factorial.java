1b4c6d0b8b41333a4946f581036c29d27ea4bf91
verytinydever <tinydever@gmail.com>
2020-07-24 21:06:06 +0545

    user input

--- Factorial.java
+++ Factorial.java
@@ -3,7 +3,10 @@ import java.util.*;
 public class Factorial {
 	public static void main (String [] args) {
 		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter number \n");
+		int number=sc.nextInt();
 		sc.close();
+		
 	}
 
 }
