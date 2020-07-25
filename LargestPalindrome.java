61f2d037f817e2413776bab35c0b6f90a961a209
verytinydever <tinydever@gmail.com>
2020-07-25 20:38:39 +0545

    constructor

--- LargestPalindrome.java
+++ LargestPalindrome.java
@@ -2,7 +2,8 @@ package com.test;
 
 public class LargestPalindrome {
 	public static void main(String [] args) {
-
+		LargestPalindrome LP=new LargestPalindrome();
+		System.out.println(""+LP.findLP("bananas"));
 	}
 
 
