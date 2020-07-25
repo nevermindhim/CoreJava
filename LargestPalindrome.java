3316fd1a4bb1b8d71d15e56677862f38d7107dbb
verytinydever <tinydever@gmail.com>
2020-07-25 20:40:26 +0545

    main logic

--- LargestPalindrome.java
+++ LargestPalindrome.java
@@ -10,7 +10,13 @@ public class LargestPalindrome {
 			return "Enter string";
 		if(s.length()==1)
 			return s;
-
+		String longest="";
+		int i=0;
+		longest=s.substring(0, 1);
+		for(i=0;i<s.length();i++) {
+			
+		}
+		return longest;
 	}
 
 }
