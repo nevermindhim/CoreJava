48ca3af662d09b06ff812c75f234f389eaac76eb
verytinydever <tinydever@gmail.com>
2020-07-25 21:26:25 +0545

    push pop

--- QueueWithTwoStack.java
+++ QueueWithTwoStack.java
@@ -3,9 +3,36 @@ package com.test;
 import java.util.Stack;
 
 public class QueueWithTwoStack {
-	public class Queue{
+	static class Queue{
 		Stack<Integer> s1;
 		Stack<Integer> s2;
+	}
+	static void push(Stack<Integer> s,int num) {
+		s.push(num);
+	}
+	static int pop(Stack<Integer> s) {
+		if (s.isEmpty()) {
+			System.out.println("Underflow");
+			System.exit(0);
+		}
+		return s.pop();
+	}
+	static void enQueue(Queue q, int x) {
+		push(q.s1,x);
+	}
+	static int deQueue(Queue q) {
+		int x=0;
+		if(q.s1.isEmpty() && q.s2.isEmpty()) {
+			System.out.println("No items in queue");
+			System.exit(1);
+		}
+		if(q.s2.isEmpty()) {
+			while(!q.s1.isEmpty()) {
+				
+			}	
+		}
+		return x;
+		
 	}
 	public static void main(String[] args) {
 		
