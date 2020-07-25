a0a84047cdb4f1edc5fb5bbb3de53cf450453516
verytinydever <tinydever@gmail.com>
2020-07-25 21:45:55 +0545

    simple hash table for integer and string

--- Hashing.java
+++ Hashing.java
@@ -1,5 +1,11 @@
 package com.test;
-
+import java.util.*;
 public class Hashing {
-
+	public static void main(String [] args) {
+		Hashtable <Integer, String> HT= new Hashtable<>();
+		HT.put(1,"Gribesh");
+		HT.put(2,"Dhakal");
+		HT.put(2,"GD");
+		System.out.println(HT);
+	}
 }
