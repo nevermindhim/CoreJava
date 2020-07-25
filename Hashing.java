0aa410ea0e718a48712e1b3b7c9cb6ae5c516206
verytinydever <tinydever@gmail.com>
2020-07-25 21:57:15 +0545

    HashMap

--- Hashing.java
+++ Hashing.java
@@ -1,11 +1,27 @@
 package com.test;
 import java.util.*;
 public class Hashing {
+	static void createHashMap(int a[]) {
+		HashMap<Integer, Integer> hMap= new HashMap<Integer, Integer>();
+		for (int i=0;i<a.length;i++) {
+			Integer c = hMap.get(a[i]);
+//			System.out.println(c);
+			if(hMap.get(a[i])==null) {
+				hMap.put(a[i],1);
+				System.out.println("If"+a[i]);
+				System.out.println(hMap);
+			}
+			else {
+				hMap.put(a[i],++c);
+				System.out.println("Else"+a[i]);
+				System.out.println(hMap);
+			}
+		}
+		System.out.println(hMap);
+	}
+	
 	public static void main(String [] args) {
-		Hashtable <Integer, String> HT= new Hashtable<>();
-		HT.put(1,"Gribesh");
-		HT.put(2,"Dhakal");
-		HT.put(2,"GD");
-		System.out.println(HT);
+		int arr[]= {10,34,5,10,3,5,10};
+		createHashMap(arr);
 	}
 }
