25423a216a8a881ff0a1150d276037016c24e1f2
verytinydever <tinydever@gmail.com>
2020-07-25 21:18:44 +0545

    class queue

--- QueueWithTwoStack.java
+++ QueueWithTwoStack.java
@@ -1,5 +1,14 @@
 package com.test;
 
+import java.util.Stack;
+
 public class QueueWithTwoStack {
+	public class Queue{
+		Stack<Integer> s1;
+		Stack<Integer> s2;
+	}
+	public static void main(String[] args) {
+		
+	}
 
 }
