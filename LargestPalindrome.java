8a0c4705eec36aeb3571a1d95689cc0152579c15
verytinydever <tinydever@gmail.com>
2020-07-25 20:49:11 +0545

    checkFunction

--- LargestPalindrome.java
+++ LargestPalindrome.java
@@ -14,9 +14,23 @@ public class LargestPalindrome {
 		int i=0;
 		longest=s.substring(0, 1);
 		for(i=0;i<s.length();i++) {
-			
-		}
-		return longest;
+			String tmp = checkForEquality(s, i, i);
+			if (tmp.length() > longest.length()) {
+                longest = tmp;
+            }
+            tmp = checkForEquality(s, i, i + 1);
+            if (tmp.length() > longest.length()) {
+                longest = tmp;
+            }
+        }
+        return longest;
 	}
+	public String checkForEquality(String s, int begin, int end) {
+        while (begin >= 0 && end <= s.length() - 1 && s.charAt(begin) == s.charAt(end)) {
+            begin--;
+            end++;
+        }
+        return s.substring(begin + 1, end);    
+    }
 
 }
