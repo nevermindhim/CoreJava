f5fa3aad92f7ba66be0f9ead2928fc81641b1832
verytinydever <tinydever@gmail.com>
2020-07-29 19:36:22 +0545

    user inpur

--- PrintAscii.java
+++ PrintAscii.java
@@ -3,7 +3,11 @@ import java.util.*;
 
 public class PrintAscii {
 	public static void main(String[] args) {
+		System.out.println("Enter character or string to find ASCII");
 		Scanner sc=new Scanner(System.in);
+		String userInputs =sc.next();
 		sc.close();
+		int ascii=0;
+		
 	}
 }
