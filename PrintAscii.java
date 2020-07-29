8287b263f73a1177bf3845ce2b50be9db5856635
verytinydever <tinydever@gmail.com>
2020-07-29 20:03:57 +0545

    Print ascii values

--- PrintAscii.java
+++ PrintAscii.java
@@ -1,6 +1,7 @@
 package com.test;
 import java.util.*;
 
+
 public class PrintAscii {
 	public static void main(String[] args) {
 		System.out.println("Enter character or string to find ASCII");
@@ -9,11 +10,14 @@ public class PrintAscii {
 		sc.close();
 		char [] userInput=userInputs.toCharArray();
 		int ascii=0;
+		int [] asciiArray =new int[userInput.length];
 		int i=0;
 		for(i=0;i<userInput.length;i++) {
 			ascii=(int) userInput[i];
+			asciiArray[i]=ascii;
 			System.out.println("ASCII of "+userInput[i]+" is "+ascii);
 		}
+		System.out.println(Arrays.toString(asciiArray));
 		
 	}
 }
