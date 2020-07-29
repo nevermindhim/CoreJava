889b4dafc20aa3731491561b9222b6721bc061be
verytinydever <tinydever@gmail.com>
2020-07-29 21:13:12 +0545

    String -->  ASCII  and sorted ASCII --> Character

--- PrintAscii.java
+++ PrintAscii.java
@@ -1,23 +1,36 @@
 package com.test;
-import java.util.*;
 
+import java.util.*;
 
 public class PrintAscii {
 	public static void main(String[] args) {
 		System.out.println("Enter character or string to find ASCII");
-		Scanner sc=new Scanner(System.in);
-		String userInputs =sc.next();
+		Scanner sc = new Scanner(System.in);
+		String userInputs = sc.next();
 		sc.close();
-		char [] userInput=userInputs.toCharArray();
-		int ascii=0;
-		int [] asciiArray =new int[userInput.length];
-		int i=0;
-		for(i=0;i<userInput.length;i++) {
-			ascii=(int) userInput[i];
-			asciiArray[i]=ascii;
-			System.out.println("ASCII of "+userInput[i]+" is "+ascii);
+		char[] userInput = userInputs.toCharArray();
+		int ascii = 0;
+		int[] asciiArray = new int[userInput.length];
+		int i = 0;
+		for (i = 0; i < userInput.length; i++) {
+			ascii = (int) userInput[i];
+			asciiArray[i] = ascii;
+			System.out.println("ASCII of " + userInput[i] + " is " + ascii);
 		}
 		System.out.println(Arrays.toString(asciiArray));
-		
+		for (i = 0; i < asciiArray.length; i++) {
+			for (int j = i + 1; j < asciiArray.length; j++) {
+				if (asciiArray[i] > asciiArray[j]) {
+					int temp = asciiArray[i];
+					asciiArray[i] = asciiArray[j];
+					asciiArray[j] = temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(asciiArray));
+		for (i = 0; i < asciiArray.length; i++) {
+			System.out.println(asciiArray[i] + " " + Character.toString((char) asciiArray[i]));
+		}
+
 	}
 }
