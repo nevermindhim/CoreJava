f09329f51e2a3799129b7e385af4bd82cee4b030
verytinydever <tinydever@gmail.com>
2020-07-29 21:33:09 +0545

    QN 18 Solution

--- ListData.java
+++ ListData.java
@@ -11,7 +11,25 @@ public class ListData {
         L1.add('d');
         L1.add('h');
         L1.add('i');
+        int[] asciiValue = new int[L1.size()];
         System.out.println(L1);
-
+        int i = 0;
+        for (i = 0; i < L1.size(); i++) {
+            asciiValue[i] = (int) L1.get(i);
+        }
+        System.out.println(Arrays.toString(asciiValue));
+        for (i = 0; i < asciiValue.length; i++) {
+            for (int j = i + 1; j < asciiValue.length; j++) {
+                if (asciiValue[i] > asciiValue[j]) {
+                    int temp = asciiValue[i];
+                    asciiValue[i] = asciiValue[j];
+                    asciiValue[j] = temp;
+                }
+            }
+        }
+        // System.out.println(Arrays.toString(asciiValue));
+        for (i = 0; i < asciiValue.length; i++) {
+            System.out.println(Character.toString((char) asciiValue[i]) + " " + asciiValue[i]);
+        }
     }
 }
