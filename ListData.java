6a496cb0b58b55f64c0d950817d0a542c62c5660
verytinydever <tinydever@gmail.com>
2020-07-29 21:25:25 +0545

    adding word to list

--- ListData.java
+++ ListData.java
@@ -1,7 +1,17 @@
 package com.test;
 
+import java.util.*;
+
 public class ListData {
     public static void main(String[] args) {
+        List<Character> L1 = new ArrayList<>();
+        L1.add('G');
+        L1.add('a');
+        L1.add('n');
+        L1.add('d');
+        L1.add('h');
+        L1.add('i');
+        System.out.println(L1);
 
     }
 }
